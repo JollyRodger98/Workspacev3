1a9e321d9ee78729cc2ed99c9b66b0f07f26633f
Tim Waldesbühl <twaldesbuehl@gmail.com>
2020-08-02 22:19:46 +0200

    expanded weather button functionality

--- js/utilityGeneral.js
+++ js/utilityGeneral.js
@@ -24,28 +24,55 @@ $(document).ready(function(){
         return digit
     }
 
+    //dd.MM.YYYY hh:mm
+    function convertUnixFull(unixTime){
+        var normalTime = new Date(unixTime * 1000);
+        normalTime = addZero(normalTime.getHours()) + ':' + addZero(normalTime.getMinutes()) + ' ' + addZero(normalTime.getDate()) + '-' + addZero(normalTime.getMonth()) + '-' + normalTime.getFullYear();
+        return normalTime
+    }
+
+    //hh:mm
+    function convertUnixTime(unixTime){
+        var normalTime = new Date(unixTime * 1000);
+        normalTime = addZero(normalTime.getHours()) + ':' + addZero(normalTime.getMinutes());
+        return normalTime
+    }
+
+
+
     $( "#buttonWeather" ).click(function() {
 
         $("#mainResultTableBody tr").remove();
         var response = APIcall("https://api.openweathermap.org/data/2.5/weather?id=2661861&appid=3110630bf875a745670e7c443681ebed&units=metric")
         //weatherHead.appendTo('#mainResultTableHead');
-        var timestamp = new Date(response.data.dt * 1000);
-        timestamp = addZero(timestamp.getDate()) + '.' + addZero(timestamp.getMonth()) + '.' + timestamp.getFullYear() + ' ' + addZero(timestamp.getHours() + ':' + addZero(timestamp.getMinutes()));
         var $tr = $('<tr>').append(
             $('<th scope="row">').text('City:'),
             $('<td>').text(response.data.name),
         ).appendTo('#mainResultTableBody');
+
+        var $tr = $('<tr>').append(
+            $('<th scope="row">').text('Timestamp:'),
+            $('<td>').text(convertUnixFull(response.data.dt)),
+        ).appendTo('#mainResultTableBody');
+
+        var $tr = $('<tr>').append(
+            $('<th scope="row" class="align-middle">').text('Weather:'),
+            $('<td>').html(`<img id="wicon" src="http://openweathermap.org/img/w/${String(response.data.weather[0].icon)}.png" alt="Weather icon"> ${response.data.weather[0].main} (${response.data.weather[0].description})`),
+        ).appendTo('#mainResultTableBody');
+
         var $tr = $('<tr>').append(
             $('<th scope="row">').text('Temperature:'),
             $('<td>').html(`${response.data.main.temp} <span>&#8451;</span>`),
         ).appendTo('#mainResultTableBody');
-        var $tr = $('<tr>').append(
-            $('<th scope="row">').text('Timestamp:'),
-            $('<td>').text(timestamp),
-        ).appendTo('#mainResultTableBody');
+
         var $tr = $('<tr>').append(
             $('<th scope="row">').text('Humidity:'),
             $('<td>').text(response.data.main.humidity + '%'),
+        ).appendTo('#mainResultTableBody');
+
+        var $tr = $('<tr>').append(
+            $('<th scope="row">').text('Sunset/Sunrise:'),
+            $('<td>').text(convertUnixTime(response.data.sys.sunset) + ' / ' + convertUnixTime(response.data.sys.sunrise)),
         ).appendTo('#mainResultTableBody');
         
     });
