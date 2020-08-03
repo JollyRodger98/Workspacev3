e1a03e1c776bfe2759ae041a96db30c715960762
Tim Waldesbühl <twaldesbuehl@gmail.com>
2020-08-03 22:54:31 +0200

    working on adding rss feed output to site

--- js/generalButtons.js
+++ js/generalButtons.js
@@ -51,6 +51,71 @@ $(document).ready(function(){
             $('<td>').text(response.data.value),
         ).appendTo('#mainResultTableBody');
         
+    });     
+
+    // Changes XML to JSON
+function xmlToJson(xml) {
+	
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+};
+
+
+    $( "#buttonMangdex" ).click(function() {
+
+        fetch('https://mangadex.org/rss/sQF2apzwyqSmWcTfVG7MY4nb8P9EtAUZ')
+        .then(response => response.text())
+        .then(data => {
+            var xmlDoc = new window.DOMParser().parseFromString(data, "text/xml")
+            var jsonText = JSON.stringify(xmlToJson(xmlDoc));
+            console.log(jsonText);
+            var jsonObj = JSON.parse(jsonText)
+            console.log(jsonObj);
+        });
+                       
+        /*
+        $("#mainResultTable tr").remove();
+        jokeHead.appendTo('#mainResultTableHead');
+
+        $.each(response.data.items, function(i, item){
+            var $tr = $('<tr>').append(
+                $('<td>').text(item.title),
+            ).appendTo('#mainResultTableBody');
+        });
+        */
+        
     });
 
     /*
