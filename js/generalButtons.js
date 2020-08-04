7952015be0fb543477c11813ebf3432bc75f3e26
Tim Waldesbühl <twaldesbuehl@gmail.com>
2020-08-04 12:51:56 +0200

    added mangadex rss feed parsing

--- js/generalButtons.js
+++ js/generalButtons.js
@@ -53,60 +53,36 @@ $(document).ready(function(){
         
     });     
 
-    // Changes XML to JSON
-function xmlToJson(xml) {
-	
-	// Create the return object
-	var obj = {};
+    function parseMangadex(jsonObj){
+        var feed = jsonObj.rss.channel.item
+        var newFeed = [];
 
-	if (xml.nodeType == 1) { // element
-		// do attributes
-		if (xml.attributes.length > 0) {
-		obj["@attributes"] = {};
-			for (var j = 0; j < xml.attributes.length; j++) {
-				var attribute = xml.attributes.item(j);
-				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-			}
-		}
-	} else if (xml.nodeType == 3) { // text
-		obj = xml.nodeValue;
-	}
+        $.each(feed, function(i, item){
 
-	// do children
-	if (xml.hasChildNodes()) {
-		for(var i = 0; i < xml.childNodes.length; i++) {
-			var item = xml.childNodes.item(i);
-			var nodeName = item.nodeName;
-			if (typeof(obj[nodeName]) == "undefined") {
-				obj[nodeName] = xmlToJson(item);
-			} else {
-				if (typeof(obj[nodeName].push) == "undefined") {
-					var old = obj[nodeName];
-					obj[nodeName] = [];
-					obj[nodeName].push(old);
-				}
-				obj[nodeName].push(xmlToJson(item));
-			}
-		}
-	}
-	return obj;
-};
+            var entry = {
+                'title': item.title["#text"],
+                'mangaLink': item.mangaLink["#text"],
+                'chapterLink': item.guid["#text"],
+                'date': item.pubDate['#text']
+            }
+
+            newFeed.push(entry);
+        });
+        return newFeed
+    }
 
 
     $( "#buttonMangdex" ).click(function() {
 
+        $("#mainResultTable tr").remove();
         fetch('https://mangadex.org/rss/sQF2apzwyqSmWcTfVG7MY4nb8P9EtAUZ')
         .then(response => response.text())
         .then(data => {
-            var xmlDoc = new window.DOMParser().parseFromString(data, "text/xml")
-            var jsonText = JSON.stringify(xmlToJson(xmlDoc));
-            console.log(jsonText);
-            var jsonObj = JSON.parse(jsonText)
-            console.log(jsonObj);
+            var jsonObj = RSStoJSON(data)
+            var feed = parseMangadex(jsonObj)
         });
                        
         /*
-        $("#mainResultTable tr").remove();
         jokeHead.appendTo('#mainResultTableHead');
 
         $.each(response.data.items, function(i, item){
